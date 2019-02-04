ed702d135f003830f2c9ebd2fdc90af175039dae
nurbakyt <nurbakyt.abdibekov@gmail.com>
2019-02-04 19:14:04 +0600

    Updated react app

--- src/AddTaskForm/AddTaskForm.js
+++ src/AddTaskForm/AddTaskForm.js
@@ -4,8 +4,8 @@ import './AddTaskForm.css';
 
 function AddTaskForm(props) {
     return <form className="addTaskForm">
-            <input className="float-left" type="text" name="name" placeholder="Add new task" value={props.task.name} onChange={props.onChangeInput}/>
-            <button className="float-right" disabled={props.isAddButtonDisabled} type="submit" onClick={props.onAddTask}>Add</button>
+            <input type="text" name="name" placeholder="Add new task" value={props.task.name} onChange={props.onChangeInput}/>
+            <button disabled={props.isAddButtonDisabled} type="submit" onClick={props.onAddTask}>Add</button>
 
     </form>
 }
